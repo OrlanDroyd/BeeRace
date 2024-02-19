97db9088726116294744b2f2fdb431d80b9069c0
OrlanDroyd <orlandroyd@gmail.com>
2024-02-19 23:26:32 +0000

    feature/feature_race (core): setup package structure

--- app/src/main/java/com/gmail/orlandroyd/beerace/core/MainActivity.kt
+++ app/src/main/java/com/gmail/orlandroyd/beerace/core/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.gmail.orlandroyd.beerace
+package com.gmail.orlandroyd.beerace.core
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.gmail.orlandroyd.beerace.ui.theme.BeeRaceTheme
+import com.gmail.orlandroyd.beerace.core.ui.theme.BeeRaceTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
