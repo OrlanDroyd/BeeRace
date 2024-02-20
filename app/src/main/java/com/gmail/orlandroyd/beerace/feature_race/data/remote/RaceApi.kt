ae5a6dc7a7de90751c38df74a4a36f479b9ac388
OrlanDroyd <orlandroyd@gmail.com>
2024-02-20 00:28:59 +0000

    feature/feature_race (data-race): - add data dto - add data mapper - update api interface - update repository

--- app/src/main/java/com/gmail/orlandroyd/beerace/feature_race/data/remote/RaceApi.kt
+++ app/src/main/java/com/gmail/orlandroyd/beerace/feature_race/data/remote/RaceApi.kt
@@ -1,5 +1,6 @@
 package com.gmail.orlandroyd.beerace.feature_race.data.remote
 
+import com.gmail.orlandroyd.beerace.feature_race.data.remote.dto.BeesDto
 import com.gmail.orlandroyd.beerace.feature_race.data.remote.dto.RaceTimeDto
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,5 +8,8 @@ import retrofit2.http.GET
 interface RaceApi {
     @GET("bees/race/duration")
     suspend fun getRaceDuration(): Response<RaceTimeDto>
+
+    @GET("bees/race/status")
+    suspend fun getBees(): Response<BeesDto>
 
 }
--- app/src/main/java/com/gmail/orlandroyd/beerace/feature_race/data/repository/RaceRepositoryImpl.kt
+++ app/src/main/java/com/gmail/orlandroyd/beerace/feature_race/data/repository/RaceRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.gmail.orlandroyd.beerace.feature_race.data.repository
 
+import com.gmail.orlandroyd.beerace.feature_race.data.mappers.BeeToDomain
 import com.gmail.orlandroyd.beerace.feature_race.data.mappers.RaceTimeToDomain
 import com.gmail.orlandroyd.beerace.feature_race.data.remote.RaceApi
 import com.gmail.orlandroyd.beerace.feature_race.data.remote.util.BaseNetworkResponse
+import com.gmail.orlandroyd.beerace.feature_race.domain.model.BeeDomainModel
 import com.gmail.orlandroyd.beerace.feature_race.domain.model.RaceTimeDomainModel
 import com.gmail.orlandroyd.beerace.feature_race.domain.repository.RaceRepository
 import com.gmail.orlandroyd.beerace.feature_race.util.Resource
@@ -12,7 +14,9 @@ import kotlinx.coroutines.flow.flow
 class RaceRepositoryImpl(
     private val api: RaceApi,
     private val raceTimeToDomain: RaceTimeToDomain,
+    private val beeToDomain: BeeToDomain,
 ) : RaceRepository, BaseNetworkResponse() {
+
     override fun getRaceTime(): Flow<Resource<RaceTimeDomainModel>> = flow {
 
         emit(Resource.Loading())
@@ -24,6 +28,22 @@ class RaceRepositoryImpl(
         } else {
             emit(Resource.Error(message = result.message.toString(), code = result.code))
         }
+    }
+
+    override fun getBees(): Flow<Resource<List<BeeDomainModel>>> = flow {
+
+        emit(Resource.Loading())
+
+        val result = safeApiCall { api.getBees() }
+        if (result.data != null) {
+            val beesDomainModel = result.data.beeList.map {
+                beeToDomain.toDomain(it)
+            }
+            emit(Resource.Success(beesDomainModel))
+        } else {
+            emit(Resource.Error(message = result.message.toString(), code = result.code))
+        }
+
     }
 
 }
